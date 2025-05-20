ef18c2e6efb12558e76732297f83b2784b23397e
Domenicop Pettinato <mimmo.pettinato@gmail.com>
2025-05-20 22:31:16 +0200

    modifica file

--- script.js
+++ script.js
@@ -131,3 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
 window.addEventListener('load', function() {
     document.body.classList.add('loaded');
 })
+const toggleBtn = document.getElementById('theme-toggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
